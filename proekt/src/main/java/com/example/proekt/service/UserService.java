324cde2ca231e6f8617c2517a1ffd8f960ac941d
K3r1ot <103976791+K3r1ot@users.noreply.github.com>
2024-09-11 21:15:58 +0200

    Delete proekt directory

--- proekt/src/main/java/com/example/proekt/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.proekt.service;
-
-import com.example.proekt.model.User;
-
-public interface UserService {
-    User registerUser(String id, String username, String password);
-    User findByUsername(String username);
-}
